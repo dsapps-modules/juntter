de64de07cdd076de8e82ace4fb043b700bc22403
Juan-Souzaa <souzajuan430@gmail.com>
2025-09-09 15:33:00 -0300

    Remove autenticação do navbar, exibindo apenas o botão de login na página pública.

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -14,49 +14,8 @@
                 <li class="nav-item"><a id="depoimentoslink" href="{{ route('checkout') }}#depoimentos" class="nav-link">Depoimentos</a></li>
                 <li class="nav-item"><a id="faqlink" href="{{ route('checkout') }}#faq" class="nav-link">FAQ</a></li>
                 
-                @auth
-                    <!-- Usuário logado -->
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-user mr-1"></i>{{ Auth::user()->name }}
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            @switch(Auth::user()->nivel_acesso)
-                                @case('super_admin')
-                                    <a class="dropdown-item" href="{{ route('super_admin.dashboard') }}">
-                                        <i class="fas fa-tachometer-alt mr-2"></i>Dashboard
-                                    </a>
-                                    @break
-                                @case('admin')
-                                    <a class="dropdown-item" href="{{ route('admin.dashboard') }}">
-                                        <i class="fas fa-tachometer-alt mr-2"></i>Dashboard
-                                    </a>
-                                    @break
-                                @case('vendedor')
-                                    <a class="dropdown-item" href="{{ route('vendedor.dashboard') }}">
-                                        <i class="fas fa-tachometer-alt mr-2"></i>Dashboard
-                                    </a>
-                                    @break
-                                @case('comprador')
-                                    <a class="dropdown-item" href="{{ route('comprador.dashboard') }}">
-                                        <i class="fas fa-tachometer-alt mr-2"></i>Dashboard
-                                    </a>
-                                    @break
-                            @endswitch
-                            <div class="dropdown-divider"></div>
-                            <form method="POST" action="{{ route('logout') }}" class="d-inline">
-                                @csrf
-                                <button type="submit" class="dropdown-item">
-                                    <i class="fas fa-sign-out-alt mr-2"></i>Sair
-                                </button>
-                            </form>
-                        </div>
-                    </li>
-                @else
-                    <!-- Usuário não logado -->
-                    <li class="nav-item"><a id="login-navbar" href="{{ route('login') }}" class="btn btn-warning ml-2 px-4">Login</a></li>
-                    <!-- Registro desabilitado -->
-                @endauth
+                <!-- Sempre mostrar apenas o botão de login na página pública -->
+                <li class="nav-item"><a id="login-navbar" href="{{ route('login') }}" class="btn btn-warning ml-2 px-4">Login</a></li>
             </ul>
         </div>
     </div>
