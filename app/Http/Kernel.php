e0290509a047a248d34912ccf1f7735964a34bbf
Juan-Souzaa <souzajuan430@gmail.com>
2025-09-11 16:08:41 -0300

    adiciona middleware MustChangePassword para redirecionar usuários que precisam trocar a senha e atualiza o Kernel para incluir a nova rota

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'nivel.acesso' => \App\Http\Middleware\NivelAcessoMiddleware::class,
+        'must.change.password' => \App\Http\Middleware\MustChangePassword::class,
     ];
 }
