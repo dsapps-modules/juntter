458c48a27bc46b4515a559f298fd43d59d1760bc
reginaldodoprado <reginaldodoprado@gmail.com>
2025-12-10 09:57:52 -0300

    Modify UserSeeder to comment out Super Admin and Vendedor
    Comment out user creation for Super Admin, Vendedor and update Admin details.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,34 +14,33 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         // Super Admin
-        $super = User::create([
-            'name' => 'Super Administrador',
-            'email' => 'superadmin@juntter.com',
-            'password' => Hash::make('12345678'),
-        ]);
-        $super->nivel_acesso = 'super_admin';
-        $super->email_verified_at = now();
-        $super->save();
+        // $super = User::create([
+        //     'name' => 'Super Administrador',
+        //     'email' => 'superadmin@juntter.com',
+        //     'password' => Hash::make('12345678'),
+        // ]);
+        // $super->nivel_acesso = 'super_admin';
+        // $super->email_verified_at = now();
+        // $super->save();
 
         // Admin
         $admin = User::create([
-            'name' => 'Administrador',
-            'email' => 'admin@juntter.com',
-            'password' => Hash::make('12345678'),
+            'name' => 'Admin DS',
+            'email' => 'admin@dsapps.com',
+            'password' => Hash::make('@5224106rP'),
         ]);
         $admin->nivel_acesso = 'admin';
         $admin->email_verified_at = now();
         $admin->save();
 
         // Vendedor
-        $vend = User::create([
-            'name' => 'Vendedor',
-            'email' => 'vendedor@juntter.com',
-            'password' => Hash::make('12345678'),
-        ]);
-        $vend->nivel_acesso = 'vendedor';
-        $vend->email_verified_at = now();
-        $vend->save();
+        // $vend = User::create([
+        //     'name' => 'Vendedor',
+        //     'email' => 'vendedor@juntter.com',
+        //     'password' => Hash::make('12345678'),
+        // ]);
+        // $vend->nivel_acesso = 'vendedor';
+        // $vend->email_verified_at = now();
+        // $vend->save();
     }
 }
-
